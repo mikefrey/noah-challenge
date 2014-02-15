3a2689ec0c4d0444dc56e3856dd1344c79fb1134
Mike Frey <frey.mike@gmail.com>
2014-02-15 11:54:49 -0600

    rediection based on whether the user is logged in or not.

--- app/middleware/loggedIn.js
+++ app/middleware/loggedIn.js
@@ -2,7 +2,7 @@ var Ballot = require('../models/ballot')
 
 module.exports = function *(next) {
   console.log('USER', this.user)
-  if (!this.user || this.user._id)
+  if (!this.user || !this.user._id)
     this.redirect('/')
   yield next
 }
--- app/routes/home.js
+++ app/routes/home.js
@@ -1,3 +1,4 @@
 module.exports = function *(next) {
+  if (this.path == '/') this.redirect('/ballot')
   this.body = yield this.render('home', { me:this.user })
 }
