2f32153110b441eea9126518f786e847a0b13040
mikefrey <frey.mike@gmail.com>
2016-01-24 16:08:13 -0600

    field validation

--- app/assets/js/controllers/register.js
+++ app/assets/js/controllers/register.js
@@ -4,12 +4,25 @@ angular.module('oscars')
   .controller('RegisterCtrl', function (UserService, $location) {
 
     this.createUser = function(user) {
+
+      if (!(user && user.firstName && user.lastName &&
+            user.password && user.email && user.invite)) {
+        return this.error = 'Please fill in all fields!'
+      }
+
+      if (!/^[^@]+@[^@]+$/.test(user.email)) {
+        return this.error = 'Email address is not in the proper format.'
+      }
+
       UserService.create(user)
         .then(function loginSuccess() {
           $location.url('/ballot')
-        }, function loginError() {
-          $location.url('/login-error')
-        })
+        }, function loginError(data) {
+          console.log(arguments)
+          if (data.error)
+            this.error = data.error
+          // $location.url('/login-error')
+        }.bind(this))
     }
 
   })
