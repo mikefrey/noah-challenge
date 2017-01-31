e50dbf83cbac5e2d7e1e8b27008ca19d21cc997a
Mike Frey <frey.mike@gmail.com>
2017-01-30 19:59:00 -0600

    updated mongo env var

--- config/default.js
+++ config/default.js
@@ -1,7 +1,7 @@
 module.exports = {
 
   mongo: {
-    url: process.env.MONGOHQ_URL || "mongodb://localhost/oscars"
+    url: process.env.MONGODB_URI
   },
 
   keys: [']pb[`k(+gf@?.[L9[D7GPA', 'yIvWTOG=nR3iVr|_m.{7wj'],
